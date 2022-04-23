07c2296b622459ba7a6f980c62d463968d3e30fe
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2022-04-24 01:56:48 +0300

    Update welcome.js

--- pages/welcome.js
+++ pages/welcome.js
@@ -72,7 +72,22 @@ const Services = () => (
                 </div>
                 </div>
             </div>
-
+            <div className="pt-6 w-full md:w-4/12 px-4 text-center">
+                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                    <div className="px-4 py-5 flex-auto">
+                        <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400">
+                          <AccessibilityNewIcon/>
+                        </div>
+                        <h6 className="text-xl font-semibold">
+                          حول
+                        </h6>
+                        <p className="mt-2 mb-4 text-gray-600 text-right">
+                          التطبيق منتج من منتجات فريق بي مبيديا تم بواسطة منصور أحمد,   
+                          يتألف بي ميديا من فريق طموح من مختلف الدول العربية
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
       </div>
     </section>
